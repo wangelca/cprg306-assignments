d891d6aba96ce5f1ea7e25a394d52d5bd1871934
Angelica Pekas <114897718+wangelca@users.noreply.github.com>
2024-01-21 16:04:14 -0700

    Update page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,1 +1,13 @@
+//Week 2 - Creating a New Page
+//Angelica Pekas CPRG-306-F
+//000906248
+import StudentInfo from "./student-info.js";
 
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
