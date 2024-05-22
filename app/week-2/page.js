ca78dd3af6527943a893fa6f50ac9f914c554b10
wangelca <angelicaverz31@gmail.com>
2024-05-21 22:56:02 -0600

    //Week 2 Assignment Changes

---
+++ app/week-2/page.js
@@ -0,0 +1,13 @@
+//Week 2 - Creating a New Page
+//Angelica Pekas CPRG-306-D
+//000906248
+import StudentInfo from "./student-info.js";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+//Week 2 - Creating and Rendering StudentInfo Component
+//Angelica Pekas CPRG-306-D
+//000906248
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Angelica Pekas</p>
+      <p>
+        <Link href="https://github.com/wangelca">Git Link</Link>
+      </p>
+    </div>
+  );
+}
